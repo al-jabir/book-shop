0d226ab5de1680ad92dc38774764608a8df540d3
al-jabir <aljabir098@gmail.com>
2024-03-12 13:53:18 +0600

    added users

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -19,4 +19,30 @@ const getUsers = async (req, res) => {
   }
 };
 
-module.exports = getUsers;
+const addUsers = async (req, res) => {
+  try {
+    const q = 'INSERT INTO users (`Name`,`Age`,`Village`,`Job`,`Salary`,`Office)';
+    const values = [
+      'name from server',
+      'age from server',
+      'village from server',
+      'job from server',
+      'salary from server',
+      'office from server',
+    ];
+    await db.query(q, [values], (err, data) => {
+      if (err) {
+        return res.status(404).json({
+          success: false,
+          message: 'Users added not successfully',
+        });
+      } else {
+        res.status(200).json({ success: true, message: 'Users added successfully', data });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+module.exports = { getUsers, addUsers };
