0a4241be92b15fe03acd0204cd9ed0089cde06a9
al-jabir <aljabir098@gmail.com>
2024-03-12 15:09:50 +0600

    crud operation done

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -19,25 +19,15 @@ const getUsers = async (req, res) => {
   }
 };
 
-const addUsers = async (req, res) => {
+const getUsersById = async (req, res) => {
   try {
-    const q = 'INSERT INTO users (`Name`,`Age`,`Village`,`Job`,`Salary`,`Office)';
-    const values = [
-      req.body.name,
-      req.body.age,
-      req.body.Village,
-      req.body.Job,
-      req.body.Salary
-      req.body.Office
-    ];
-    await db.query(q, [values], (err, data) => {
+    const id = req.params.id;
+    const q = 'select * from users where Id = ?';
+    await db.query(q, id, (err, data) => {
       if (err) {
-        return res.status(404).json({
-          success: false,
-          message: 'Users added not successfully',
-        });
+        return res.status(400).json({ success: false, message: 'User id data not found' });
       } else {
-        res.status(200).json({ success: true, message: 'Users added successfully', data });
+        return res.status(200).json({ success: true, message: 'User id data found', data });
       }
     });
   } catch (err) {
@@ -45,4 +35,71 @@ const addUsers = async (req, res) => {
   }
 };
 
-module.exports = { getUsers, addUsers };
+const addUsers = async (req, res) => {
+  try {
+    const q = 'INSERT INTO users (`Name`,`Age`,`Village`,`Job`,`Salary`,`Office`) VALUES (?,?,?,?,?,?)';
+    const { Name, Age, Village, Job, Salary, Office } = req.body;
+    const values = [Name, Age, Village, Job, Salary, Office];
+    await db.query(q, values, (err, data) => {
+      if (err) {
+        return res.status(404).json({
+          success: false,
+          message: 'Users added not successfully',
+        });
+      } else {
+        res.status(200).json({ success: true, message: 'Users added successfully' });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+const updateUsers = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { Name, Age, Village, Job, Salary, Office } = req.body;
+    const dUpdate = [Name, Age, Village, Job, Salary, Office, userId];
+    const q = 'UPDATE users SET Name = ?, Age = ?,Village = ?,Job = ?,Salary = ?,Office = ? WHERE Id = ?';
+    await db.query(q, dUpdate, (err, data) => {
+      if (err) {
+        return res.status(404).json({
+          success: false,
+          message: 'User updated not successfully',
+        });
+      } else {
+        return res.status(200).json({
+          success: true,
+          message: 'user updated successfully',
+          data,
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+const deleteUsers = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const q = 'DELETE FROM users WHERE Id = ?';
+    await db.query(q, userId, (err, data) => {
+      if (err) {
+        return res.status(404).json({
+          success: false,
+          message: 'User deleted not successfully',
+        });
+      } else {
+        return res.status(200).json({
+          success: true,
+          message: 'User deleted successfully',
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+module.exports = { getUsers, getUsersById, addUsers, updateUsers, deleteUsers };
--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,8 +1,11 @@
-const { getUsers, addUsers } = require('../controllers/usersController');
+const { getUsers, addUsers, getUsersById, deleteUsers, updateUsers } = require('../controllers/usersController');
 
 const usersRoute = require('express').Router();
 
 usersRoute.get('/users', getUsers);
+usersRoute.get('/users/:id', getUsersById);
 usersRoute.post('/users', addUsers);
+usersRoute.put('/users/:id', updateUsers);
+usersRoute.delete('/users/:id', deleteUsers);
 
 module.exports = usersRoute;
