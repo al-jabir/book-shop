4cd69e7c048a469ceceb3fa161258db5c545451c
al-jabir <aljabir098@gmail.com>
2024-03-12 13:45:28 +0600

    add users post

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,7 +1,8 @@
-const getUsers = require('../controllers/usersController');
+const { getUsers, addUsers } = require('../controllers/usersController');
 
 const usersRoute = require('express').Router();
 
 usersRoute.get('/users', getUsers);
+usersRoute.post('/users', addUsers);
 
 module.exports = usersRoute;
