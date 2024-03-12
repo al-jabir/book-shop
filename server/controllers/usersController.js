a301b528b939526e5d7df8fb88cf74ac7b79a5a6
al-jabir <aljabir098@gmail.com>
2024-03-12 13:43:22 +0600

    get data successfully

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,5 +1,22 @@
 const db = require('../db/users');
 
-const getUsers = async (req, res) => {};
+const getUsers = async (req, res) => {
+  try {
+    const q = 'SELECT * FROM users';
+    await db.query(q, (err, data) => {
+      if (err) {
+        return res.status(404).json({ success: false, message: 'users data not found' });
+      } else {
+        return res.status(200).json({
+          success: true,
+          message: 'users data all successfully',
+          data,
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
 
 module.exports = getUsers;
