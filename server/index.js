63ba85fd2886e55b1e49bae5ea23e3a1ac1c044b
al-jabir <aljabir098@gmail.com>
2024-03-12 12:56:20 +0600

    error handlers

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+// error handlers
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'page not found',
+  });
+});
+
+// server
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });
